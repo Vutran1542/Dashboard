099921a6e2f1906f2708b94179a3ae5eb5f6727b
Vutran1542 <144546128+Vutran1542@users.noreply.github.com>
2023-09-27 14:00:11 +0700

    Add files via upload

---
+++ order.js
@@ -0,0 +1,80 @@
+const Dataorder = [
+    {
+        productName: 'DJI Mavic 3 Pro',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping:'Pending'
+    },
+    {
+        productName: 'DJI RC Pro',
+        productNumber: '36631',
+        paymentStatus: 'Refunded',
+        shipping:'Declined'
+    },
+    {
+        productName: 'DJI Mini 3 pro',
+        productNumber: '8331',
+        paymentStatus: 'Due',
+        shipping:'Declined'
+    },
+    {
+        productName: 'Fly P11 Pro Max',
+        productNumber: '23331',
+        paymentStatus: 'Paid',
+        shipping:'Delivered'
+    },
+    {
+        productName: 'DJI Mini 2 SE',
+        productNumber: '22331',
+        paymentStatus: 'Refunded',
+        shipping:'Pending'
+    },
+    {
+        productName: 'E88 Drone',
+        productNumber: '85631',
+        paymentStatus: 'Paid',
+        shipping:'Pending'
+    },
+    {
+        productName: 'DJI Air 2S',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping:'Pending'
+    },
+    {
+        productName: 'QuadronX Drone',
+        productNumber: '87631',
+        paymentStatus: 'Refunded',
+        shipping:'Delivered'
+    },
+    {
+        productName: 'FlyCam K918',
+        productNumber: '23331',
+        paymentStatus: 'Refunded',
+        shipping:'Delivered'
+    },
+    {
+        productName: 'DJI Mini 3 Pro Smart',
+        productNumber: '34531',
+        paymentStatus: 'Due',
+        shipping:'Pending'
+    },
+    {
+        productName: 'DJI Mini 3',
+        productNumber: '43321',
+        paymentStatus: 'Paid',
+        shipping:'Declined'
+    },
+    {
+        productName: 'Flycam I9 Max',
+        productNumber: '33431',
+        paymentStatus: 'Refunded',
+        shipping:'Delivered'
+    },
+    {
+        productName: 'FlycamSJRC F11S 4K Pro',
+        productNumber: '44631',
+        paymentStatus: 'Paid',
+        shipping:'Delivered'
+    }
+]
